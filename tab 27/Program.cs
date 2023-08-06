57731b61454c02db6343181d8faa1d96eff4637a
Ksusha1996 <123461531+Ksusha1996@users.noreply.github.com>
2023-08-06 19:21:27 +0000

    tab 27

---
+++ tab 27/Program.cs
@@ -0,0 +1,32 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int number = int.Parse(Console.ReadLine());
+
+        int sum = CalculateDigitSum(number);
+        Console.WriteLine($"Сумма цифр: {sum}");
+    }
+
+    static int CalculateDigitSum(int number)
+    {
+        int sum = 0;
+        while (number != 0)
+        {
+            int digit = number % 10;
+            sum += digit;
+            number /= 10;
+        }
+        return sum;
+    }
+}
